e4bc7cff9608886f65e28b2a986990084bef67e5
arielxxgreen <ariel.green@bison.howard.edu>
2026-02-26 16:55:37 -0500

    Implemented printing from beginning to start indec -1 for forward rotation

--- proj0.s
+++ proj0.s
@@ -11,3 +11,17 @@ main:
 
     li $v0, 10
     syscall
+
+forward_part2:
+    la $t6, id
+
+forward_part3:
+    add $t7, $t5, $t4
+    beq $t6, $t7, forward_finish
+
+    lb $a0, 0($t6)
+    li $v0, 11
+    syscall
+
+    addi $t6, $t6, 1
+    j forward_part3
